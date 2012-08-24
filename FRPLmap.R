ad51c632398255aaa9b347274d58bba1b518a164
Jared Knowles <jknowles@gmail.com>
2012-08-24 12:02:37 -0500

    a

--- FRPLmap.R
+++ FRPLmap.R
@@ -8,7 +8,8 @@
 # Version 0.7
 # Date: 08/21/2012
 # Author: Jared E. Knowles, Policy Research Advisor DPI
-# Data sources: WINSS, wisemaps (http://wisemaps.dpi.wi.gov)
+# Data sources: WINSS (http://data.dpi.state.wi.us/data/)
+# WISEemaps (http://wisemaps.dpi.wi.gov)
 
 
 # Setup packages
