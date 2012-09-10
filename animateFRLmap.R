8af06896091be7782a72a4b90ad10021bd8d7926
Jared Knowles <jknowles@gmail.com>
2012-09-10 15:18:41 -0500

    Animate with GraphicsMagick

--- animateFRLmap.R
+++ animateFRLmap.R
@@ -8,3 +8,21 @@
 # Data sources: WINSS (http://data.dpi.state.wi.us/data/)
 # WISEemaps (http://wisemaps.dpi.wi.gov)
 
+library(animation)
+# Need GraphicsMagick installed and in the path
+
+setwd("plots")
+# Avoid weird output in animation package
+ani.options(outdir=getwd(),tempdir=getwd())
+
+# Loop repeatedly in GIF
+ani.options(loop = TRUE)
+# May need to specify full path here
+gm.convert("F:/FRLMap/plots/evenFRLmap*.png",output="evenFRLmap.gif")
+
+ani.options(loop = TRUE)
+# Loop once and then stop
+ani.options(loop=1)
+# May need to specify full path here
+gm.convert("F:/FRLMap/plots/evenFRLmap*.png",output="evenFRLmap_noloop.gif")
+
