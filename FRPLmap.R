76bbbe0b967e939a6cb1d868c532945bd3f7d3e7
Jared Knowles <jknowles@gmail.com>
2014-08-28 10:37:32 -0500

    updates for 2015

--- FRPLmap.R
+++ FRPLmap.R
@@ -89,11 +89,12 @@ download.file("http://dl.dropbox.com/u/1811289/shapefile/publicshapefileUHS.prj"
 
 # Read in shapefile
 fn<-paste(getwd(),"/shapefile/publicshapefileUHS",sep="")
-distall<-readShapePoly(fn)
+distall<-readShapePoly(fn, IDvar="CODE")
 
 # Read in map themes
-source('ggplot2themes.R')
+# source('ggplot2themes.R')
 
+test <- mapmerge(distall, frl_w, xid="CODE", yid="district_number")
 
 
 #Merge
@@ -104,10 +105,11 @@ y<-y[order(y$district_number),]
 di<-merge(y,d,by.x="district_number",by.y="CODE",all.y=TRUE)
 di<-di[order(di$sort),]
 ##Drop extraneous data##
-di$sort2<-di$sort-1
-row.names(di)<-di$sort2
+# di$sort2<-di$sort-1
+# row.names(di)<-di$sort2
 #row.names(di)<-di$sort
-distall2<-spCbind(distall,di)
+row.names(di) <- di$district_number
+distall2<-spCbind(distall, di)
 #rm(y,VAmath)
 
 # Make the object back into a data frame we can plot with ggplot2 methods
@@ -147,7 +149,7 @@ for(i in 2001:2012){
                         scale_fill_manual(values=cols3)+
                         geom_text(data=cnames,aes(x=long,y=lat,label=distname),size=.95)+
                         guides(fill = guide_legend(title='% FRL',ncol=1))+
-                        opts(title='Proportion of Students FRL ",i,"')+
+                        labs(title='Proportion of Students FRL ",i,"')+
                         theme_dpi_mapPNG()",sep="")))
   eval(parse(text=paste("png('plots/evenFRLmap",i,".png',height=1200,width=1000)",sep="")))
   eval(parse(text=paste("print(ineq3",i,")",sep="")))
@@ -160,7 +162,7 @@ cols3<-c('Less than 10%'='#F1EEf6','10-20%'='#D4B9DA','20-30%'='#C994C7','30-40%
          '40-50%'='#DD1C77','More than 50%'='#980043')
 
 # Make PDF files
-
+library(eeptools)
 for(i in 2001:2012){
   eval(parse(text=paste("ineq3",i,"<-ggplot(data=df.points,aes(long,lat))+
   geom_polygon(aes(group=group,fill=cut3.",i,"))+
@@ -169,7 +171,7 @@ for(i in 2001:2012){
                         scale_fill_manual(values=cols3)+
                         geom_text(data=cnames,aes(x=long,y=lat,label=distname),size=.95)+
                         guides(fill = guide_legend(title='% FRL',ncol=1))+
-                        opts(title='Proportion of Students FRL ",i,"')+
+                        labs(title='Proportion of Students FRL ",i,"')+
                         theme_dpi_map2()",sep="")))
   eval(parse(text=paste("pdf('plots/evenFRLmap",i,".pdf',height=10,width=8)",sep="")))
   eval(parse(text=paste("print(ineq3",i,")",sep="")))
